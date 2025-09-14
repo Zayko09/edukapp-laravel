1f6c20733fd8b8f3b822a95c1f3fcadb1ac1cc49
jhonata rubiano <jhonatanvrubiano@gmail.com>
2025-09-13 20:10:37 -0500

    si

--- app/Http/Controllers/Usuario/UsuarioController.php
+++ app/Http/Controllers/Usuario/UsuarioController.php
@@ -6,15 +6,15 @@ use App\Models\Usuario;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class usuarioController extends Controller
+class UsuarioController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
     public function index()
     {  
-         $Usuario = usuario::all();
-        return view('usuario.index',compact('usuario'));
+         $Usuario = Usuario::all();
+        return view('Usuario.index',compact('Usuario'));
     }
 
     /**
